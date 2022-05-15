893687c2f84004bae309b25c56f5b69b5ae8c9bf
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2022-05-15 11:21:53 -0400

    Create loop.cs

---
+++ loop.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Windows.Forms;
+public class Program {
+    static void Main(string[] args) {
+      for (i = 0; i < 5; i++) {
+        MessageBox.Show("Hello", "World", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+    }
+}
